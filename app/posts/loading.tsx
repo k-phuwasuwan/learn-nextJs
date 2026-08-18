3a106a8eeb74f1f53ac36ebe2658f77bb8f01dfb
k-phuwasuwan <kantawit.phu@gmail.com>
2026-08-18 21:18:53 +0700

    loading UI

---
+++ app/posts/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <main className="p-8">
+        <p className="text-lg">Loading...</p>
+    </main>
+  )
+}
+
+export default loading
