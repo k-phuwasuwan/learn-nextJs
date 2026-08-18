25388fafd0878270ac699dc0287c3e6394576331
k-phuwasuwan <kantawit.phu@gmail.com>
2026-08-18 21:35:08 +0700

    feat: contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,6 +10,7 @@ function ContactPage() {
   return (
     <main className="p-8">
       <h1 className="text-3xl font-bold">Contact Page</h1>
+      <p className="mt-4">Welcome to Contact Page</p>
     </main>
   )
 }
