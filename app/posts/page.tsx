ac7c2ecd61cecc559d9a4eace4b83f21e456bbdc
k-phuwasuwan <kantawit.phu@gmail.com>
2026-08-18 21:16:23 +0700

    data fetching

---
+++ app/posts/page.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+type Post = {
+    id: number,
+    title: string,
+    body: string
+}
+
+async function getPosts(): Promise<Post[]> {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts',{
+        cache: 'no-store'
+    })
+    if (!res.ok) {
+        throw new Error('Failed to fetch posts')
+    }
+    return res.json()
+}
+
+async function PostsPage() {
+    const posts = await getPosts()
+  return (
+    <main className="p-8">
+        <h1 className="text-3xl font-bold"></h1>
+        <div className="mt-6 grid gap-4">
+            {posts.slice(0, 10).map((post) => {
+                return (
+                    <article key={post.id}
+                    className="rounded-lg border p-4">
+                        <h2 className="text-xl font-semibold">{post.title}</h2>
+                        <p className="mt-2 text-gray-600">{post.body}</p>
+                    </article> 
+                )
+            })}
+        </div>
+    </main>
+  )
+}
+
+export default PostsPage
